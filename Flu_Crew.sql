2af8c364d594cc61653a777b6ac74538cb1d6377
enjbaez <enjoli.baez@gmail.com>
2020-03-08 18:41:24 -0400

    pop_data load and sqltable update

--- Flu_Crew.sql
+++ Flu_Crew.sql
@@ -1,30 +1,29 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/d1lyl5
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/kUU1sP
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
 CREATE TABLE "Influenza_Data" (
-    "REGION" VARCHAR   NOT NULL,
-    "YEAR" DATE   NOT NULL,
-    "AGE 0-4" INTEGER   NOT NULL,
-    "AGE 25-49" VARCHAR   NOT NULL,
-    "AGE 25-64" INTEGER   NOT NULL,
-    "AGE 5-24" INTEGER   NOT NULL,
-    "AGE 50-64" INTEGER   NOT NULL,
-    "AGE 65" INTEGER   NOT NULL,
-    "ILITOTAL" INTEGER   NOT NULL,
-    "TOTAL PATIENTS" INTEGER   NOT NULL
+    "REGION" VARCHAR,
+    "YEAR" DATE,
+    "AGE 0-4" INTEGER,
+    "AGE 25-49" VARCHAR,
+    "AGE 25-64" INTEGER,
+    "AGE 5-24" INTEGER,
+    "AGE 50-64" INTEGER,
+    "AGE 65" INTEGER,
+    "ILITOTAL" INTEGER,
+    "TOTAL PATIENTS" INTEGER
 );
 
 CREATE TABLE "Population_Data" (
-    "Region" VARCHAR   NOT NULL,
-    "NAME" VARCHAR   NOT NULL,
-    "POPESTIMATE2018" INTEGER   NOT NULL,
+    "Region" VARCHAR,
+    "NAME" VARCHAR,
+    "POPESTIMATE2018" INTEGER,
     CONSTRAINT "pk_Population_Data" PRIMARY KEY (
-        "Region"
+        "NAME"
      )
 );
 
 ALTER TABLE "Influenza_Data" ADD CONSTRAINT "fk_Influenza_Data_REGION" FOREIGN KEY("REGION")
-REFERENCES "Population_Data" ("Region");
-
+REFERENCES "Population_Data" ("NAME");
